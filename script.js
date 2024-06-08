1915790b1bf9991c1a12c52b79facaf8702db0a0
SabinaGarayeva <sgarayeva13@gmail.com>
2024-06-08 16:20:05 +0100

    lazy loading images

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ const revealSection = function (entries, observer) {
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove("section--hidden");
-  observer.unobserve(entry.target)
+  observer.unobserve(entry.target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -135,4 +135,31 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 allSections.forEach((section) => {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
+});
+
+//lazy loading images
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  //Replace src- with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+});
+
+imgTargets.forEach((img) => {
+  imgObserver.observe(img);
 });
